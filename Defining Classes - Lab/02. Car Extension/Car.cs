4ebe536b4bdc9ee8ee5933ed13ba9d63ce0e683e
Vik-Vel <vikiveleva54@gmail.com>
2023-02-17 08:42:29 +0200

    Defining Classes - Lab

---
+++ Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+
+
+        public string Make { get { return make; } set { this.make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int Year { get { return this.year; } set { this.year = value; } }
+        public double FuelQuantity { get { return fuelQuantity; } set { fuelQuantity = value; } }
+        public double FuelConsumption { get { return fuelConsumption; } set { fuelConsumption = value; } }
+
+
+        public void Drive(double distance)
+        {
+            double neededFuel = distance - fuelConsumption;
+
+            if (neededFuel < fuelConsumption)
+            {
+                fuelQuantity -= neededFuel;
+            }
+            else
+            {
+                Console.WriteLine($"Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.AppendLine($"Fuel: {FuelQuantity}");
+
+            return result.ToString();   
+        }
+    }
+}
+
