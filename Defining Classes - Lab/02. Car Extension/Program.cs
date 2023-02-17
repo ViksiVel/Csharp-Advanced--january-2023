afded43d767b8213cd13c3302945a1efb0af790b
Vik-Vel <vikiveleva54@gmail.com>
2023-02-17 09:51:46 +0200

    Uploading right file

---
+++ Defining Classes - Lab/02. Car Extension/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            
+            car.Drive(10);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
---
+++ Defining Classes - Lab/SpecialCars/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialCars
+{
+    public class Engine
+    {
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+
+        public int GetHorsePower(string[] splitted)
+        {
+            int horsePower = int.Parse(splitted[0]);
+
+            return horsePower;
+        }
+
+        public double GetCubicCapacity(string[] splitted)
+        {
+            double cubicCapacity = double.Parse(splitted[1]);
+
+            return cubicCapacity;
+        }
+    }
+}
