f9b219bf45eeacc7b5b5786890f80fb9575ac9be
Vik-Vel <vikiveleva54@gmail.com>
2023-03-31 10:55:06 +0300

    Iterators and Comparators - Lab

---
+++ Iterators and Comparators - Lab/01.Library/Library.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books) //tyk da ochakva mnojestvo elementi, za tova e params
+        {
+            Books = books.ToList();
+        }
+        public Library()
+        {
+            Books = new List<Book>(); 
+        }
+        public List<Book> Books { get; set; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return Books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
